a3928f2670d76c460657bed855aa10b11e821ab9
Tommy Jackson <tom.jackson50@gmail.com>
2019-02-16 12:19:36 +0000

    changed requirerments file

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ def hello():
     return "hell world .. again"
 
 if __name__ == "__name__":
-    app.run(host=os.getenv("IP, 0.0.0.0"),
-        port=int(os.getenv(PORT, 8080)),
+    app.run(host=os.getenv('IP'),
+        port=int(os.getenv('PORT')),
         debug=True)
