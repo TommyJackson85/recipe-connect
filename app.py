dbdabcc69812c2d7c4fc1c13d56081e22aba83e6
Tommy Jackson <tom.jackson50@gmail.com>
2019-03-28 01:03:56 +0000

    attempting to fix a bug2

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ print(sys.path)
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'recipe-manager'
-app.config["MONGO_URI"] = 'MONGOURI'
+app.config["MONGO_URI"] = os.getenv('MONGOURI')
 #app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 
